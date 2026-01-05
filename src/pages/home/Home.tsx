8b51918f2d9055bd42d14df03b535a5b00a9bbe5
Abdulaziz <kamolov607@gmail.com>
2026-01-05 13:38:06 +0500

    first commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div className="container">
+        <div className="bg-red-500 text-5xl text-white" > 
+            this is home
+        </div>
+        <div>
+          <Button>
+            this is button
+          </Button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
