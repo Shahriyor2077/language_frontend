54b64faa21951248642bebb105a2b0321dfb4a21
Shahriyor2077 <zshahriyor40@gmail.com>
2026-01-18 00:01:28 +0500

    u

--- src/config/request.ts
+++ src/config/request.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+
 const request = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: API_URL,
 });
 
 request.interceptors.request.use((config) => {
